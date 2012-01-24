d91983c70a7e8e6828b1d5dbe3d765180a6722ce
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-01-23 21:58:07 -0700

    rubywarrior, completed levels 1 and 2, onto 3

--- project02/rubywarrior/ineeda-warrior-beginner/player.rb
+++ project02/rubywarrior/ineeda-warrior-beginner/player.rb
@@ -1,5 +1,10 @@
 class Player
   def play_turn(warrior)
-    # add your code here
-  end
-end
+    # add your code here - Will DO! **Alyse Haynes**
+	if(warrior.feel.empty?)
+		warrior.walk!
+	elsif
+		warrior.attack!
+	end # end 'if' statement
+  end # end play_turn 
+end #end class Player
