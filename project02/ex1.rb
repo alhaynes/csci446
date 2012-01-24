0d08ce43d6da952752ed6fb587bd84b952ee722f
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-01-23 21:33:18 -0700

    Just finished Exercise01 extra credit

--- project02/ex1.rb
+++ project02/ex1.rb
@@ -5,3 +5,18 @@ puts "I know."
 puts 'Yey! Printing.'
 puts "Well, I'd much rather you 'not'."
 puts 'Nah Nah Nah Nah "cant touch this" mmmhmm.'
+
+# Extra Credit (1)
+
+puts "\n"
+puts "Printing another line - oh snap."
+
+# Extra Credit (2) 
+# To make it print only one line, I would place a '#' at the begininng of the lines I 
+# did not want to print
+
+# Extra Credit (3)
+# using a '#' comments an entire line to the end, so it would end here -> (now) 
+
+# Extra Credit (4)
+# Deal.
