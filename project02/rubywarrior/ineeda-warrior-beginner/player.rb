1ad92f46c6b719709bad4f3671201bb4b1b43ed3
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-01-23 22:13:59 -0700

    Completed Level 3, onto the next level

--- project02/rubywarrior/ineeda-warrior-beginner/player.rb
+++ project02/rubywarrior/ineeda-warrior-beginner/player.rb
@@ -1,10 +1,16 @@
 class Player
   def play_turn(warrior)
     # add your code here - Will DO! **Alyse Haynes**
-	if(warrior.feel.empty?)
+	
+	if warrior.feel.empty? and warrior.health < 20
+		warrior.rest!
+		
+	elsif warrior.feel.empty? and warrior.health >= 20
 		warrior.walk!
+	
 	elsif
 		warrior.attack!
 	end # end 'if' statement
+  
   end # end play_turn 
 end #end class Player
