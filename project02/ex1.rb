fe3c6aa65159617d0bcaf52f2044ada7a9859618
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-01-23 21:22:41 -0700

    Just finished 'A good first program'  and it was indeed.

---
+++ project02/ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Oh, Hello Alyse"
+puts "I like typing this."
+puts "I know."
+puts 'Yey! Printing.'
+puts "Well, I'd much rather you 'not'."
+puts 'Nah Nah Nah Nah "cant touch this" mmmhmm.'
