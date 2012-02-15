3daee68793e0ba55464141107d2ec49c4e2a75bb
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-02-14 18:08:09 -0700

    Fixing project03 so that it adhears to the grading criteria

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '00937604186baa8a0a1d0f2eb39d924a9eb39d60d5889197183c49967d33b3a3cb43ece83be4bfcbaa70b75313febb38da11e2f1823e2bdbaed0698290076d3c'
