e80120d3f94b2286d18fac1e8f7d755417454def
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-02-24 17:18:30 -0700

    Added more to the article page, have links and icons for view, edit and delete

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.delete_all
+
+Article.create(title: 'Man this article is sweet',
+author: 'Alyse Haynes',
+body: %{
+	This article about articles is probably the sweetest article you'll ever read. 
+	Articles have a very article quality about them that you'll find extremly articly.
+	An article article is something that every article should have. Article.
+	})
+	
