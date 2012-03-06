2cb87aacc25a70ffd7fdac2da8003edad36d4b71
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-03-06 00:12:49 -0700

    Finished! I think the seed data is correct for both authors and articles, and I also realized that after you create an article the author name information needed to be updated so that it didnt show a pointer so I fixed that too

--- project05/db/seeds.rb
+++ project05/db/seeds.rb
@@ -10,44 +10,55 @@ Article.delete_all
 
 article_attributes =[
 	{ :title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.' },
 	{ :title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.'},
 	{:title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.' },
 	{:title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.' },
 	{:title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.'},
 	{:title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.' },
 	{:title => 'Man this article is sweet',
+	:author_id => '1',
 		:body =>
 	'This article about articles is probably the sweetest article youll ever read. 
 	Articles have a very article quality about them that youll find extremly articly.
 	An article article is something that every article should have. Article.' }
 	]
-
 	
 	article_attributes.each do |attribute|
 		Article.create!(attribute)
 	end
 	
+	author_attributes = [ {:name => 'Bark Rezington'}]
+	
+	author_attributes.each do |attribute|
+		Author.create!(attribute)
+	end
