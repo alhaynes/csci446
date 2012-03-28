fe570e19aa75a5aa1ae84be762b86788f92b92d9
Alyse Haynes <alhaynes@mymail.mines.edu>
2012-03-28 17:31:02 -0600

    Adding project06 directory with gamez

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '704ad1de5040c1dde11442f2f98a93971516b9175f9fb9c10485b4bf46a890ee0b53dd0142747dc5190eab0cf31326817e4a3c9fa9f1432431854921cd28556a'
